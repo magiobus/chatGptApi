421c00aa50d3d06587e8662f50daef67ff375ddc
magiobus <magio@magiobus.com>
2022-12-11 15:57:00 -0600

    Try Catch, but token is still not working

--- pages/api/chatgpt/input.js
+++ pages/api/chatgpt/input.js
@@ -2,36 +2,38 @@
 import { ChatGPTAPI } from "chatgpt";
 
 export default async function handler(req, res) {
+  if (req.method !== "POST")
+    return res.status(405).json({ error: "Method not allowed" });
+
   const { title } = req.body;
 
   if (!title) return res.status(400).json({ error: "title is required" });
 
-  const message = `I have a video for youtube titled "${title}"
+  const message = `I have a video titled "${title}"
 can you give me a prompt for stable difussion to generate the thumbnail background image ?`;
 
-  const api = new ChatGPTAPI({
-    sessionToken: process.env.SESSION_TOKEN,
-    markdown: false,
-  });
+  try {
+    const api = new ChatGPTAPI({
+      sessionToken: process.env.SESSION_TOKEN,
+      markdown: false,
+    });
+    // // ensure the API is properly authenticated
+    await api.ensureAuth();
+    const response = await api.sendMessage(message);
 
-  // ensure the API is properly authenticated
-  const ensureAuth = await api.ensureAuth();
+    // //search for the first ":" in the response and return the text after it
+    const index = response.indexOf(":");
+    const prompt = response.substring(index + 1);
 
-  if (!ensureAuth)
-    return res.status(500).json({ error: "Error session token" });
+    // //Clean prompt so it does not contain the chars " and /n and /t and  \
+    const promptClean = prompt.replaceAll('"', "");
+    const promptClean2 = promptClean
+      .replaceAll("\n", "")
+      .replaceAll("\t", "")
+      .replaceAll("\\", "");
 
-  // send a message and wait for the response
-  const response = await api.sendMessage(message);
-  //search for the first ":" in the response and return the text after it
-  const index = response.indexOf(":");
-  const prompt = response.substring(index + 1);
-
-  //Clean prompt so it does not contain the chars " and /n and /t and  \
-  const promptClean = prompt.replaceAll('"', "");
-  const promptClean2 = promptClean
-    .replaceAll("\n", "")
-    .replaceAll("\t", "")
-    .replaceAll("\\", "");
-
-  res.status(200).json(promptClean2);
+    res.status(200).json(promptClean2);
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
 }
