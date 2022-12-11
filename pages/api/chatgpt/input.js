4c1fdc660a94071ce2875af7e8310a3d6a3b3ad8
magiobus <magio@magiobus.com>
2022-12-10 21:49:52 -0600

    Parsing response from chatgpt

--- pages/api/chatgpt/input.js
+++ pages/api/chatgpt/input.js
@@ -7,8 +7,7 @@ export default async function handler(req, res) {
   if (!title) return res.status(400).json({ error: "title is required" });
 
   const message = `I have a video for youtube titled "${title}"
-can you give me a prompt for stable difussion to generate the thumbnail background image ?
-Just show me the prompt.  `;
+can you give me a prompt for stable difussion to generate the thumbnail background image ?`;
 
   const api = new ChatGPTAPI({
     sessionToken: process.env.SESSION_TOKEN,
@@ -16,10 +15,13 @@ Just show me the prompt.  `;
   });
 
   // ensure the API is properly authenticated
-  await api.ensureAuth();
+  const ensureAuth = await api.ensureAuth();
 
   // send a message and wait for the response
   const response = await api.sendMessage(message);
+  //search for the first ":" in the response and return the text after it
+  const index = response.indexOf(":");
+  const prompt = response.substring(index + 1);
 
-  res.status(200).json(response);
+  res.status(200).json(prompt);
 }
